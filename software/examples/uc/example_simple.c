5f8329ea4ef393799d8c532e95a22e057905c3a9
Erik Fleckstein <erik@tinkerforge.com>
2022-04-19 16:03:29 +0200

    examples: uc: Rename TF_HalContext to TF_HAL.

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -9,19 +9,19 @@
 
 void check(int rc, const char* msg);
 
-void example_setup(TF_HalContext *hal);
-void example_loop(TF_HalContext *hal);
+void example_setup(TF_HAL *hal);
+void example_loop(TF_HAL *hal);
 
 
 static TF_IndustrialDigitalOut4V2 ido4;
 
-void example_setup(TF_HalContext *hal) {
+void example_setup(TF_HAL *hal) {
 	// Create device object
 	check(tf_industrial_digital_out_4_v2_create(&ido4, UID, hal), "create device object");
 
 	// Set channels alternating high/low 10 times with 100 ms delay
 	int i;
-	for(i = 0; i < 10; ++i) {
+	for (i = 0; i < 10; ++i) {
 		tf_hal_sleep_us(hal, 100 * 1000);
 		bool value[4] = {true, false, false, false};
 		check(tf_industrial_digital_out_4_v2_set_value(&ido4, value), "call set_value");
@@ -37,7 +37,7 @@ void example_setup(TF_HalContext *hal) {
 	}
 }
 
-void example_loop(TF_HalContext *hal) {
+void example_loop(TF_HAL *hal) {
 	// Poll for callbacks
 	tf_hal_callback_tick(hal, 0);
 }
