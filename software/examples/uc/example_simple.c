62e812d082bf42d47797ada74802c7f9eeb157b3
Matthias Bolte <matthias@tinkerforge.com>
2022-04-27 11:50:29 +0200

    uc: Adapt to bindings changes

--- software/examples/uc/example_simple.c
+++ software/examples/uc/example_simple.c
@@ -1,23 +1,19 @@
 // This example is not self-contained.
-// It requres usage of the example driver specific to your platform.
+// It requires usage of the example driver specific to your platform.
 // See the HAL documentation.
 
-#include "bindings/hal_common.h"
-#include "bindings/bricklet_industrial_digital_out_4_v2.h"
+#include "src/bindings/hal_common.h"
+#include "src/bindings/bricklet_industrial_digital_out_4_v2.h"
 
-#define UID "XYZ" // Change XYZ to the UID of your Industrial Digital Out 4 Bricklet 2.0
-
-void check(int rc, const char* msg);
-
+void check(int rc, const char *msg);
 void example_setup(TF_HAL *hal);
 void example_loop(TF_HAL *hal);
-
 
 static TF_IndustrialDigitalOut4V2 ido4;
 
 void example_setup(TF_HAL *hal) {
 	// Create device object
-	check(tf_industrial_digital_out_4_v2_create(&ido4, UID, hal), "create device object");
+	check(tf_industrial_digital_out_4_v2_create(&ido4, NULL, hal), "create device object");
 
 	// Set channels alternating high/low 10 times with 100 ms delay
 	int i;
